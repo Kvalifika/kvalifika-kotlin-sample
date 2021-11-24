07abb3207433970cc17aa94bd4464239f37af67e
Jayesh Mardiya <mardiyajayesh@gmail.com>
2021-11-24 16:22:59 +0530

    feat: upgrade to 1.0.1

--- app/src/main/java/com/kvalifika/kotlinapp/MainActivity.kt
+++ app/src/main/java/com/kvalifika/kotlinapp/MainActivity.kt
@@ -1,27 +1,27 @@
 package com.kvalifika.kotlinapp
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.kvalifika.sdk.KvalifikaSDK
 import com.kvalifika.sdk.KvalifikaSDKCallback
 import com.kvalifika.sdk.KvalifikaSDKError
 import com.kvalifika.sdk.KvalifikaSDKLocale
-import org.json.JSONObject
 
 class MainActivity : AppCompatActivity() {
     private lateinit var sdk: KvalifikaSDK
-    private val appId: String = "YOUR APP ID"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val appId = ""
         sdk = KvalifikaSDK.Builder(this, appId)
-                .locale(KvalifikaSDKLocale.GE)
-                .build()
+            .locale(KvalifikaSDKLocale.GE)
+            .build()
 
         sdk.callback(object : KvalifikaSDKCallback {
             override fun onInitialize() {
@@ -41,48 +41,104 @@ class MainActivity : AppCompatActivity() {
             override fun onError(error: KvalifikaSDKError, message: String?) {
                 if (error == KvalifikaSDKError.INVALID_APP_ID) {
                     runOnUiThread {
-                        Toast.makeText(applicationContext, "Invalid App ID", Toast.LENGTH_LONG).show()
+                        Toast.makeText(applicationContext, "Invalid App ID", Toast.LENGTH_LONG)
+                            .show()
                     }
                 }
 
                 if (error == KvalifikaSDKError.USER_CANCELLED) {
-                    Toast.makeText(applicationContext, "User cancelled", Toast.LENGTH_LONG).show()
+                    runOnUiThread {
+                        Toast.makeText(applicationContext, "User cancelled", Toast.LENGTH_LONG)
+                            .show()
+                    }
                 }
 
                 if (error == KvalifikaSDKError.TIMEOUT) {
-                    Toast.makeText(applicationContext, "Timeout", Toast.LENGTH_LONG).show()
+                    runOnUiThread {
+                        Toast.makeText(applicationContext, "Timeout", Toast.LENGTH_LONG).show()
+                    }
                 }
 
                 if (error == KvalifikaSDKError.SESSION_UNSUCCESSFUL) {
-                    Toast.makeText(applicationContext, "Session failed", Toast.LENGTH_LONG).show()
+                    runOnUiThread {
+                        Toast.makeText(applicationContext, "Session failed", Toast.LENGTH_LONG)
+                            .show()
+                    }
                 }
 
                 if (error == KvalifikaSDKError.ID_UNSUCCESSFUL) {
-                    Toast.makeText(applicationContext, "ID scan failed", Toast.LENGTH_LONG).show()
+                    runOnUiThread {
+                        Toast.makeText(applicationContext, "ID scan failed", Toast.LENGTH_LONG)
+                            .show()
+                    }
                 }
 
                 if (error == KvalifikaSDKError.CAMERA_PERMISSION_DENIED) {
-                    Toast.makeText(applicationContext, "Camera permission denied", Toast.LENGTH_LONG).show()
+                    runOnUiThread {
+                        Toast.makeText(
+                            applicationContext,
+                            "Camera permission denied",
+                            Toast.LENGTH_LONG
+                        ).show()
+                    }
                 }
 
                 if (error == KvalifikaSDKError.LANDSCAPE_MODE_NOT_ALLOWED) {
-                    Toast.makeText(applicationContext, "Landscape mode is not allowed", Toast.LENGTH_LONG).show()
+                    runOnUiThread {
+                        Toast.makeText(
+                            applicationContext,
+                            "Landscape mode is not allowed",
+                            Toast.LENGTH_LONG
+                        ).show()
+                    }
                 }
 
                 if (error == KvalifikaSDKError.REVERSE_PORTRAIT_NOT_ALLOWED) {
-                    Toast.makeText(applicationContext, "Reverse portrait is not allowed", Toast.LENGTH_LONG).show()
+                    runOnUiThread {
+                        Toast.makeText(
+                            applicationContext,
+                            "Reverse portrait is not allowed",
+                            Toast.LENGTH_LONG
+                        ).show()
+                    }
                 }
 
                 if (error == KvalifikaSDKError.FACE_IMAGES_UPLOAD_FAILED) {
-                    Toast.makeText(applicationContext, "Could not upload face images", Toast.LENGTH_LONG).show()
+                    runOnUiThread {
+                        Toast.makeText(
+                            applicationContext,
+                            "Could not upload face images",
+                            Toast.LENGTH_LONG
+                        ).show()
+                    }
                 }
 
                 if (error == KvalifikaSDKError.DOCUMENT_IMAGES_UPLOAD_FAILED) {
-                    Toast.makeText(applicationContext, "Could not upload Id card or passport images", Toast.LENGTH_LONG).show()
+                    runOnUiThread {
+                        Toast.makeText(
+                            applicationContext,
+                            "Could not upload Id card or passport images",
+                            Toast.LENGTH_LONG
+                        ).show()
+                    }
+                }
+
+                if (error == KvalifikaSDKError.NO_MORE_ATTEMPTS) {
+                    runOnUiThread {
+                        val intent = Intent(applicationContext, MainActivity::class.java)
+                        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+                        startActivity(intent)
+                    }
                 }
 
                 if (error == KvalifikaSDKError.UNKNOWN_INTERNAL_ERROR) {
-                    Toast.makeText(applicationContext, "Unknown error happened: $message", Toast.LENGTH_LONG).show()
+                    runOnUiThread {
+                        Toast.makeText(
+                            applicationContext,
+                            "Unknown error happened: $message",
+                            Toast.LENGTH_LONG
+                        ).show()
+                    }
                 }
             }
         })
